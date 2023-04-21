b3695f9a16ae98885fd5fb8f32fe94718ac00c9c
mbrky6 <mberkey6@ivytech.edu>
2023-04-20 20:16:05 -0400

    Added a script and object to allow tiles to be selected Currently, selecting just means changing the color, but more code can be easily added for more functionality

---
+++ Assets/Scripts/Selection.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Selection : MonoBehaviour {
+
+    public Material highlightMaterialYes; // Material of valid hovered object
+    public Material highlightMaterialNo; // Material of invalid hovered object
+    public Material selectedMaterial; // Material of selected object
+
+    private Material originalMaterialHighlight; // Original material of highlighted object
+    private Material originalMaterialSelection; // Original material of selected object
+    private bool valid; // Checks the validity of the hover (Not currently in use)
+    private Transform highlight; // Transform for highlighting
+    private Transform selection; // Transform for selecting
+    private RaycastHit raycastHit; // Raycast to determine what is being pointed at
+
+    // Update is called once per frame
+    void Update() {
+        // Returning to original Material
+        if (highlight != null) {
+            highlight.GetComponent<MeshRenderer>().material = originalMaterialHighlight;
+            highlight = null;
+        } // if (highlighted)
+
+        // Highlighting
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); // Draw a ray from the mouse position
+        if (!EventSystem.current.IsPointerOverGameObject() && Physics.Raycast(ray, out raycastHit)) {
+            highlight = raycastHit.transform;
+            if (highlight.CompareTag("tile") && highlight != selection) {
+                if (highlight.GetComponent<MeshRenderer>().material != highlightMaterialYes) {
+                    originalMaterialHighlight = highlight.GetComponent<MeshRenderer>().material;
+                    highlight.GetComponent<MeshRenderer>().material = highlightMaterialYes;
+                } // if (highlighted object does not have the highlighted material)
+            } // if (highlighted object is a tile and not selected)
+            else {
+                highlight = null;
+            } // else
+        } // if (mouse is over anything other than a tile)
+
+        // Selecting
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject()) {
+            if (highlight) {
+                if (selection != null) {
+                    selection.GetComponent<MeshRenderer>().material = originalMaterialSelection;
+                } // if (object is selected)
+                selection = raycastHit.transform;
+                if (selection.GetComponent<MeshRenderer>().material != selectedMaterial) {
+                    originalMaterialSelection = originalMaterialHighlight;
+                    selection.GetComponent<MeshRenderer>().material = selectedMaterial;
+                } // if (clicked object is a tile)
+                highlight = null;
+            } // if (the object is highlighted)
+            else {
+                if (selection) {
+                    selection.GetComponent<MeshRenderer>().material = originalMaterialSelection;
+                    selection = null;
+                } // if (object is selected)
+            } // else
+        } // if (highlighted object clicked)
+    } // void Update
+} // + class Selection
